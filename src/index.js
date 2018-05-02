4459fc39c724827cae278dabbe3278088e215f41
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-05-02 12:53:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+// no idea where we r supposed to put this code... probs don't copy. 
+var profile = (
+  <div>
+    <img src="avatar.png" className="profile" />
+    <h3>{[user.firstName, user.lastName].join(' ')}</h3>
+  </div>;
+)
